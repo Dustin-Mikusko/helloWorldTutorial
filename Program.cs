5f31e2723671907c45855a546eb7c32afe981d6e
Dustin Mikusko <dustinjm713@yahoo.com>
2019-11-26 12:30:48 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloWorldTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
